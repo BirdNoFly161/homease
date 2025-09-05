6586c5cb78131333ec6d7512a7cd0fb2c516facf
Oussama <sharp.oussama@gmail.com>
2025-09-05 16:47:35 +0100

    database schema and a playground file to test

--- api/database/config.js
+++ api/database/config.js
@@ -1,7 +1,11 @@
 const mongodb_user_name = "sharpoussama";
 const mongodb_user_password = "rC90rht0ZA9dKRtf";
-const database_name = "pressingshop"
+const database_name = "homease"
 
 //export const uri = `mongodb+srv://${mongodb_user_name}:${mongodb_user_password}@testcluster.amyjknf.mongodb.net/${database_name}?retryWrites=true&w=majority`;
 
 export const uri = `mongodb+srv://${mongodb_user_name}:${mongodb_user_password}@cluster0.gx7stm3.mongodb.net/${database_name}?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// for atlas extension
+const static_uri= `mongodb+srv://sharpoussama:rC90rht0ZA9dKRtf@cluster0.gx7stm3.mongodb.net/`
